2eb3feaf21cff219f1cedd95cef4ad4c03945e6a
suhascv <suhazcsk@gmail.com>
2020-12-13 10:28:44 -0500

    users logs fetch

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 //mongo connection
 mongoose.Promise = global.Promise;
-cosnt mongoURI=""
+const mongoURI="";
 mongoose.connect(mongoURI, 
 { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
@@ -25,7 +25,8 @@ const LogSchema = new Schema({
     type:Number
   },
   date:{
-    type:Date
+    type:Date,
+    required:false
   }
 }) 
 const ExerciseTrackerSchema = new Schema(
@@ -35,7 +36,7 @@ const ExerciseTrackerSchema = new Schema(
       unique:true,
       required:true
     },
-    logs:[LogSchema],
+    log:[LogSchema],
   }
 );
 
@@ -56,6 +57,7 @@ app.get('/', (req, res) => {
 
 
 app.post('/api/exercise/new-user',parser,(req,res)=>{
+
   let newUser = new ExerciseTracker(
     {username:req.body.username,
     logs:[]
@@ -66,7 +68,7 @@ app.post('/api/exercise/new-user',parser,(req,res)=>{
       res.send("invalid username or username already taken!");
     }
     else{
-      console.log(user);
+      //console.log(user);
       res.json(
           {username:req.body.username,
           _id:user._id._id
@@ -76,12 +78,14 @@ app.post('/api/exercise/new-user',parser,(req,res)=>{
 });
 
 app.post('/api/exercise/add',parser,(req,res)=>{
+  console.log(req.body);
+  let date1 = req.body.date?req.body.date:new Date();
   let newLog = {
     description:req.body.description,
     duration:req.body.duration,
-    date:req.body.date
+    date:date1
   }
-  console.log(req.body.userId);
+  //console.log(req.body.userId);
 
   if(req.body.userId.length!==24){
 
@@ -100,13 +104,19 @@ app.post('/api/exercise/add',parser,(req,res)=>{
       if(resp){
       ExerciseTracker.findOneAndUpdate(
         {_id:mongoose.Types.ObjectId(req.body.userId)},
-        {"$push":{logs:newLog}},
+        {"$push":{log:newLog}},
         (err,resp1)=>{
           if(err){
             res.send("invalid exercise/log, please provide data in required format");
           }
           else{
-            res.json(req.body);
+            res.json({
+              _id:resp1._id,
+              username:resp1.username,
+              description:req.body.description,
+              duration:req.body.duration,
+              date:req.body.date?req.body.date:new Date().toDateString()
+            });
           }
         }
         ) 
@@ -117,12 +127,74 @@ app.post('/api/exercise/add',parser,(req,res)=>{
     }
   }
   )
-  
-    
-  }
 
+
+  }
 });
+
+
+app.get('/api/exercise/users',(req,res)=>{
+  ExerciseTracker.find({},{_id:1,username:1},(err,users)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      //console.log(users);
+      res.json(users)
+    }
+  })
+});
+
+app.get('/api/exercise/log',(req,res)=>{
+  
+  if(req.query.userId){
+      let query={_id:req.query.userId}
+      let limit=100;
+      if(req.query.from){
+        query['date']={'$gte':req.query.from}
+      }
+      if(req.query.to){
+        query['date']['$lte']=req.query.to
+      }
+      console.log(query)
+      if(req.query.limit){
+        limit=req.query.limit
+      }
+
+      ExerciseTracker.findOne(
+        query,
+        {_id:1,username:1,log:1},
+      (err,logs)=>{
+        if(err){
+          res.send(err)
+        }
+        else{
+        if(logs){
+        console.log(logs)
+        res.json({
+          _id:logs._id,
+          username:logs.username,
+          log:logs.log.slice(limit),
+          count:logs.log.length
+        });}
+        
+        else{
+          res.json({
+            _id:req.query.userId,
+            log:[],
+          count:0
+          })
+        }
+        }
+      })
+    }
+  
+})
+
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
